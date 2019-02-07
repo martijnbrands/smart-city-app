3a1b08deca22acc1efee55261cdb61f5987b587b
martijnbrands <nijtrambra@hotmail.com>
2019-02-07 11:32:11 +0100

    Fullscreen camera in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
-  <v-app>
-     <router-view></router-view>
-  </v-app>
+  <a-scene embedded arjs>
+  	<a-marker preset="hiro">
+            <a-box position='0 0.5 0' material='color: black;'></a-box>
+  	</a-marker>
+  	<a-entity camera></a-entity>
+    </a-scene>
 </template>
 
 <script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,5 @@
-
 <template>
   <div>
-    <a-scene embedded arjs>
-  	  <a-marker preset="hiro">
-        <a-box position="0 0.5 0" material="color: black;"></a-box>
-  	  </a-marker>
-  	  <a-entity camera></a-entity>
-    </a-scene>
   </div>
 </template>
 
