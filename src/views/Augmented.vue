184582c41da9bce29e6246af4e0304b8e008755c
martijnbrands <nijtrambra@hotmail.com>
2019-02-12 12:14:17 +0100

    remove Aframe embedded

--- src/views/Augmented.vue
+++ src/views/Augmented.vue
@@ -1,8 +1,8 @@
 <template>
-            <a-scene embedded vr-mode-ui="enabled: false" arjs="trackingMethod: best; sourceType: webcam; debugUIEnabled: false;">
-            <a-marker preset="hiro">
+    <a-scene vr-mode-ui="enabled: false" arjs='sourceType: webcam; debugUIEnabled: false;'>
+        <a-marker preset="hiro">
             <a-sphere position="0 0.5 0" radius="1.25" color="#EF2D5E" shadow></a-sphere>
-            </a-marker>
-            <a-entity camera></a-entity>
-            </a-scene>
+        </a-marker>
+        <a-entity camera></a-entity>
+    </a-scene>
 </template>
