7e1161cf28e2f1095cac9549fb83eaaddc6b02d9
martijnbrands <nijtrambra@hotmail.com>
2019-02-12 10:30:00 +0100

    Remove VR ui button

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
- <a-scene embedded arjs="trackingMethod: best; sourceType: webcam; debugUIEnabled: false;">
+ <a-scene embedded vr-mode-ui="enabled: false" arjs="trackingMethod: best; sourceType: webcam; debugUIEnabled: false;">
   	<a-marker preset="hiro">
       <a-sphere position="0 0.5 0" radius="1.25" color="#EF2D5E" shadow></a-sphere>
   	</a-marker>
