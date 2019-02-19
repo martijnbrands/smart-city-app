b741380533fa4af07818c4791f03200514ee3579
martijnbrands <nijtrambra@hotmail.com>
2019-02-19 15:59:36 +0100

    Tracking mode best

--- src/components/Aframe.vue
+++ src/components/Aframe.vue
@@ -1,12 +1,12 @@
 <template>
-    <a-scene embedded vr-mode-ui="enabled: false" arjs='sourceType: webcam; debugUIEnabled: false;'>
+    <a-scene embedded vr-mode-ui="enabled: false" arjs='sourceType: webcam; debugUIEnabled: false; trackingMethod: best;'>
         <!-- define your markers -->
         <a-marker preset='hiro'>
         <!-- here define the content to display on top of the marker -->
             <a-box position='0 0.5 0' material='color: pink;'></a-box>
         </a-marker>
         <!-- define a simple camera -->
-        <a-entity camera></a-entity>
+        <a-camera-static/>
     </a-scene>
 </template>
 
