f5b4bbcc1288571c50c362cae779775018f331a9
martijnbrands <nijtrambra@hotmail.com>
2019-02-19 16:20:30 +0100

    Try to fix box on marker

--- src/components/Aframe.vue
+++ src/components/Aframe.vue
@@ -2,14 +2,12 @@
     <a-scene embedded vr-mode-ui="enabled: false" arjs='sourceType: webcam; debugUIEnabled: false; trackingMethod: best;'>
         <!-- define your markers -->
         <a-marker preset='hiro'>
-        <!-- here define the content to display on top of the marker -->
-            <a-box position='0 0.5 0' material='color: pink;'></a-box>
+            <a-box position='0 0.5 0' material='color: green;'></a-box>
         </a-marker>
         <!-- define a simple camera -->
-        <a-camera-static/>
+        <a-entity camera></a-entity>
     </a-scene>
 </template>
 
 <style lang="scss">
-    a-scene { display: block; width: 300px; height: 300px;}
 </style>
